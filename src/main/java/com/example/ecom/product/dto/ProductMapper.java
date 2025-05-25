a15d959d02e86eb1bad4e54acc893b36ab5af945
Nanret123 <nanretgungshik@gmail.com>
2025-05-25 14:10:11 +0100

    fix: ignore null values

--- src/main/java/com/example/ecom/product/dto/ProductMapper.java
+++ src/main/java/com/example/ecom/product/dto/ProductMapper.java
@@ -3,10 +3,11 @@ package com.example.ecom.product.dto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import com.example.ecom.model.Product;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ProductMapper {
   ProductResponseDto toDto(Product product);
 
