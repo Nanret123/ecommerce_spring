014782ed1632ac8f3c0290300dcaeeb03dd9b036
Nanret123 <nanretgungshik@gmail.com>
2025-05-26 18:52:17 +0100

    feat: add cart model

---
+++ src/main/java/com/example/ecom/model/Cart.java
@@ -0,0 +1,52 @@
+package com.example.ecom.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "carts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private UUID id;
+
+  private UUID userId;
+
+  @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+  private List<CartItem> cartItems;
+
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+  @PrePersist
+  protected void onCreate() {
+    createdAt = LocalDateTime.now();
+    updatedAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    updatedAt = LocalDateTime.now();
+  }
+}
