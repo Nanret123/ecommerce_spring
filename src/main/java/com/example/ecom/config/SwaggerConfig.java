f7a2d76a5a01531512c7351adb032ee8f0ad0b1e
Nanret123 <nanretgungshik@gmail.com>
2025-07-26 13:46:10 +0100

    feat: add render url

--- src/main/java/com/example/ecom/config/SwaggerConfig.java
+++ src/main/java/com/example/ecom/config/SwaggerConfig.java
@@ -1,18 +1,30 @@
 package com.example.ecom.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
-   @Bean
+  @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .info(new Info()
                     .title("Ecommerce API")
                     .version("1.0")
                     .description("Detailed API documentation with Swagger and Springdoc"));
+        openAPI.setServers(Arrays.asList(
+            new Server().url("http://localhost:8080")
+                .description("Development Server"),
+            new Server().url("https://ecommerce-spring-i3hj.onrender.com")
+                .description("Render Deployment")));
+        openAPI.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"));
+        return openAPI;
     }
 }
